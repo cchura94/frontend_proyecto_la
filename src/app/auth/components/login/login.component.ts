db52e4abab6c30924c0b90c6e35c2fb4bbac816a
cchura94 <cchura.cpc@gmail.com>
2022-11-10 19:21:42 -0400

    Http Client Login

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+  })
+
+  constructor(private authService: AuthService, private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  ingresar(){
+    // peticion al servidor
+    this.authService.login(this.loginForm.value).subscribe(
+      (res: any) => {
+        console.log(res)
+        localStorage.setItem("access_token", res.accessToken)
+
+        this.router.navigate(["/admin/perfil"])
+
+      },
+      (error: any) => {
+        console.log(error)
+
+      }
+    )
+
   }
 
 }
